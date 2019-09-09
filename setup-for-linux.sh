652491bf1183852f1916cda8ac00da216d2cc248
rio-kim <rio.dykim@gmail.com>
2019-09-09 18:10:41 +0900

    Update setup-for-linux.sh

--- setup-for-linux.sh
+++ setup-for-linux.sh
@@ -1,4 +1,4 @@
 docker-compose down
 docker-compose up -d
 sleep 5
-docker exec -it mongo1 bash -c "mongo admin -u sa -p qwe123 < ./data/replicaSet.js"
+docker exec -i mongo1 bash -c "mongo admin -u sa -p qwe123 < ./data/replicaSet.js"
